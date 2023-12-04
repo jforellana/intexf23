a6a500021c750e78c2b4b36dc5bd9becbb0b22e0
Juan Orellana <mrjfob@byu.edu>
2023-12-04 10:33:16 -0700

    initial upload

---
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sudo certbot -n -d provomentalhealth.is404.net --nginx --agree-tos --email mrjfob@byu.edu
---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+let app = express();
+
+let path  = require('path');
+
+const port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// const knex = require('knex') ({
+//     client: 'pg',
+//     connection: {
+//         host: 'localhost',
+//         user: 'postgres',
+//         password: 'password',
+//         database: 'htwf',
+//         port: 5432
+//     }
+// })
+
+app.get('/', (req,res) => {
+    res.render('index');
+});
+
+app.listen(port, () => console.log('Intex is listening'));
