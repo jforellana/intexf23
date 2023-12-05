d4fb50c930273f6983d9ba04604f378ee55bd7c3
Juan Orellana <mrjfob@byu.edu>
2023-12-05 13:16:23 -0700

    add pagination

--- index.js
+++ index.js
@@ -33,11 +33,27 @@ app.get('/survey', (req,res) => {
 });
 
 app.get('/database', (req,res) => {
-    let query = knex.select().from('main_db');
+    let pagelimit = req.body.limit || 7;
+
+    let query = knex.select().from('main_db').limit(pagelimit);
     query.toString();
     query.then(db => {
         res.render('database', {db:db});
     })
 });
+
+app.post('/database', (req, res) => {
+    let perpage = req.body.limit || 5;
+    let page = req.body.page || 1;
+    if (page < 1) page = 1;
+    let offset = (page - 1) * perpage;
+
+    let query = knex.select().from('main_db').limit(perpage).offset(offset);
+    query.toString();
+    query.then(db => {
+        res.render('database', {db:db});
+    })
+})
+
 
 app.listen(port, () => console.log('Intex is listening'));
