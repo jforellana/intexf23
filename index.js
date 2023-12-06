0a920c25bfacba09b08712606348a647492c670c
Adam Lyons <AdamLyons.BYU@gmail.com>
2023-12-05 21:01:33 -0700

    yup

--- index.js
+++ index.js
@@ -30,14 +30,21 @@ app.post("/post-survey", (req, res) => {
   let date = new Date();
 
   knex("main_db").insert({
-      Date: date.toISOString().split('T')[0] + ' ' + date.toTimeString().split(' ')[0],
-      Age: req.body.age,
-      Gender: req.body.gender,
-      Affiliation_No: req.body.affiliation,
-      Platform_No: req.body.platforms
-  })
-  knex("survey_db").insert({
-      Date: date.toISOString().split('T')[0] + ' ' + date.toTimeString().split(' ')[0],
+    Date:
+      date.toISOString().split("T")[0] +
+      " " +
+      date.toTimeString().split(" ")[0],
+    Age: req.body.age,
+    Gender: req.body.gender,
+    Affiliation_No: req.body.affiliation,
+    Platform_No: req.body.platforms,
+  });
+  knex("survey_db")
+    .insert({
+      Date:
+        date.toISOString().split("T")[0] +
+        " " +
+        date.toTimeString().split(" ")[0],
       Age: req.body.age,
       Gender: req.body.gender,
       Rel_Status: req.body.relstatus,
@@ -55,34 +62,35 @@ app.post("/post-survey", (req, res) => {
       Seek_Validation: req.body.q9,
       Depressed_Freq: req.body.q10,
       Interest_Fluctuation: req.body.q11,
-      Sleep_Issues: req.body.q12
-  }).then(mydata => {
+      Sleep_Issues: req.body.q12,
+    })
+    .then((mydata) => {
       res.redirect("/");
-  })
+    });
 });
 
-app.get('/database', (req,res) => {
-    let pagelimit = req.body.limit || 7;
+app.get("/database", (req, res) => {
+  let pagelimit = req.body.limit || 7;
 
-    let query = knex.select().from('main_db').limit(pagelimit);
-    query.toString();
-    query.then(db => {
-        res.render('database', {db:db});
-    })
+  let query = knex.select().from("main_db").limit(pagelimit);
+  query.toString();
+  query.then((db) => {
+    res.render("database", { db: db });
+  });
 });
 
-app.post('/database', (req, res) => {
-    let perpage = req.body.limit || 5;
-    let page = req.body.page || 1;
-    if (page < 1) page = 1;
-    let offset = (page - 1) * perpage;
+app.post("/database", (req, res) => {
+  let perpage = req.body.limit || 5;
+  let page = req.body.page || 1;
+  if (page < 1) page = 1;
+  let offset = (page - 1) * perpage;
 
-    let query = knex.select().from('main_db').limit(perpage).offset(offset);
-    query.toString();
-    query.then(db => {
-        res.render('database', {db:db});
-    })
-})
+  let query = knex.select().from("main_db").limit(perpage).offset(offset);
+  query.toString();
+  query.then((db) => {
+    res.render("database", { db: db });
+  });
+});
 
 app.get("/survey", (req, res) => {
   res.render("survey");
@@ -90,6 +98,10 @@ app.get("/survey", (req, res) => {
 
 app.get("/login", (req, res) => {
   res.render("login");
+});
+
+app.get("/createAccount", (req, res) => {
+  res.render("createAccount");
 });
 
 app.listen(port, () => console.log("Intex is listening"));
